cac84a5302be25a50116160d77d2e9c0403256d0
alexvicen <1bhavicente@gmail.com>
2016-06-29 14:30:06 +0200

    primeros pasos

--- app/src/main/java/com/multimedia/aes/gestnet_sgsv2/nucleo/Login.java
+++ app/src/main/java/com/multimedia/aes/gestnet_sgsv2/nucleo/Login.java
@@ -1,15 +1,27 @@
 package com.multimedia.aes.gestnet_sgsv2.nucleo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.multimedia.aes.gestnet_sgsv2.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this,Index.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/multimedia/aes/gestnet_sgsv2/nucleo/index.java
+++ app/src/main/java/com/multimedia/aes/gestnet_sgsv2/nucleo/index.java
@@ -13,7 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class index extends AppCompatActivity
+import com.multimedia.aes.gestnet_sgsv2.R;
+
+public class Index extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -22,15 +24,6 @@ public class index extends AppCompatActivity
         setContentView(R.layout.index);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
