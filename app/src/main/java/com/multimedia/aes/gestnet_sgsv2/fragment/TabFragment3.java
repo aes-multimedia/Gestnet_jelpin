489c3fa48d0208caceec29bd506e4af1a0f66d72
QuantumA <ac28042@gmail.com>
2016-07-07 14:00:29 +0200

    fragment

--- app/src/main/java/com/multimedia/aes/gestnet_sgsv2/fragment/TabFragment3.java
+++ app/src/main/java/com/multimedia/aes/gestnet_sgsv2/fragment/TabFragment3.java
@@ -5,14 +5,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.multimedia.aes.gestnet_sgsv2.R;
 
 
 public class TabFragment3 extends Fragment {
+    private View vista;
+    private Spinner estadoVisita,tipoVisita;
+    private EditText codBarras,observaciones;
+    private CheckBox contadorInterno,reparacion;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+
+        vista = inflater.inflate(R.layout.tab_fragment_3, container, false);
+
+        estadoVisita = (Spinner) vista.findViewById(R.id.spEstado_visita);
+        tipoVisita = (Spinner) vista.findViewById(R.id.spTipo_visita);
+        codBarras = (EditText) vista.findViewById(R.id.etCodigo_barras);
+        observaciones = (EditText) vista.findViewById(R.id.etObservaciones);
+        contadorInterno = (CheckBox) vista.findViewById(R.id.cbContador_interno);
+        reparacion = (CheckBox) vista.findViewById(R.id.cbReparacion);
+
+
+
+
+
+        return vista;
     }
+
+
+
+
 }
