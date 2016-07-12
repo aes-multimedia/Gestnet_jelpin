cace9f437a995a14b299df5970837d0f0af62a86
alexvicen <1bhavicente@gmail.com>
2016-07-12 09:02:23 +0200

    primeros pasos

--- app/src/main/java/com/multimedia/aes/gestnet_sgsv2/clases/arrayListDatos.java
+++ app/src/main/java/com/multimedia/aes/gestnet_sgsv2/clases/arrayListDatos.java
@@ -1,7 +1,4 @@
 package com.multimedia.aes.gestnet_sgsv2.clases;
 
-/**
- * Created by Sala5 on 04/07/2016.
- */
 public class arrayListDatos {
 }
--- app/src/main/java/com/multimedia/aes/gestnet_sgsv2/fragment/TabFragment3.java
+++ app/src/main/java/com/multimedia/aes/gestnet_sgsv2/fragment/TabFragment3.java
@@ -2,11 +2,9 @@ package com.multimedia.aes.gestnet_sgsv2.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
@@ -14,28 +12,29 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.j256.ormlite.table.DatabaseTable;
 import com.multimedia.aes.gestnet_sgsv2.R;
-import com.multimedia.aes.gestnet_sgsv2.dao.MarcaCalderaDAO;
-import com.multimedia.aes.gestnet_sgsv2.dao.PotenciaDAO;
-import com.multimedia.aes.gestnet_sgsv2.dao.TipoCalderaDAO;
+import com.multimedia.aes.gestnet_sgsv2.SharedPreferences.GestorSharedPreferences;
+import com.multimedia.aes.gestnet_sgsv2.dao.MantenimientoDAO;
 import com.multimedia.aes.gestnet_sgsv2.dao.TiposReparacionesDAO;
-import com.multimedia.aes.gestnet_sgsv2.dao.UsoCalderaDAO;
 import com.multimedia.aes.gestnet_sgsv2.entities.Mantenimiento;
 import com.multimedia.aes.gestnet_sgsv2.entities.TiposReparaciones;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.sql.SQLException;
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 
 public class TabFragment3 extends Fragment {
     private View vista;
-    private Spinner estadoVisita,tipoVisita,tipoReparacion,tiempoReparacion;
-    private EditText codBarras,observaciones,costeMateriales,manoObra;
-    private CheckBox contadorInterno,reparacion;
-    private DatePicker fechaReparacion;
-    private Button botonFinalizar;
+    private Spinner spEstadoVisita, spTipoVisita, spTipoReparacion, spTiempoReparacion;
+    private EditText etCodBarras, etObservaciones, etCosteMateriales, etManoObra;
+    private CheckBox cbContadorInterno, cbReparacion;
+    private DatePicker dpFechaReparacion;
+    private Button btnFinalizar;
     private List<TiposReparaciones> tiposReparacion;
     private String[] tipos;
     private TextView tvFechaVisita,tvFechaLimite,tvUrgencia;
@@ -44,21 +43,60 @@ public class TabFragment3 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         vista = inflater.inflate(R.layout.tab_fragment_3, container, false);
+        try {
+            JSONObject jsonObject = GestorSharedPreferences.getJsonMantenimiento(GestorSharedPreferences.getSharedPreferencesMantenimiento(getContext()));
+            int id = jsonObject.getInt("id");
+            mantenimiento = MantenimientoDAO.buscarMantenimientoPorId(getContext(),id);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        spEstadoVisita = (Spinner) vista.findViewById(R.id.spEstadoVisita);
+        spTipoVisita = (Spinner) vista.findViewById(R.id.spTipoVisita);
+        spTipoReparacion = (Spinner) vista.findViewById(R.id.spTipoReparacion);
+        spTiempoReparacion = (Spinner) vista.findViewById(R.id.spTiempoReparacion);
+        etCodBarras = (EditText) vista.findViewById(R.id.etCodigoBarras);
+        etObservaciones = (EditText) vista.findViewById(R.id.etObservaciones);
+        etCosteMateriales = (EditText) vista.findViewById(R.id.etCosteMateriales);
+        etManoObra = (EditText) vista.findViewById(R.id.etCosteManoDeObra);
+        cbContadorInterno = (CheckBox) vista.findViewById(R.id.cbContadorInterno);
+        cbReparacion = (CheckBox) vista.findViewById(R.id.cbReparacion);
+        dpFechaReparacion = (DatePicker) vista.findViewById(R.id.dpFechaReparacion);
+        btnFinalizar =(Button) vista.findViewById(R.id.btnFinalizar);
+        tvFechaVisita = (TextView)vista.findViewById(R.id.tvFechaVisita);
+        tvFechaLimite = (TextView)vista.findViewById(R.id.tvFechaLimite);
+        tvUrgencia = (TextView)vista.findViewById(R.id.tvUrgencia);
 
-        estadoVisita = (Spinner) vista.findViewById(R.id.spEstado_visita);
-        tipoVisita = (Spinner) vista.findViewById(R.id.spTipo_visita);
-        codBarras = (EditText) vista.findViewById(R.id.etCodigo_barras);
-        observaciones = (EditText) vista.findViewById(R.id.etObservaciones);
-        contadorInterno = (CheckBox) vista.findViewById(R.id.cbContador_interno);
-        reparacion = (CheckBox) vista.findViewById(R.id.cbReparacion);
-        tipoReparacion = (Spinner) vista.findViewById(R.id.spTipo_reparacion);
-        tiempoReparacion = (Spinner) vista.findViewById(R.id.spTiempo_reparacion);
-        costeMateriales= (EditText) vista.findViewById(R.id.etCosteMateriales);
-        manoObra= (EditText) vista.findViewById(R.id.etCosteManoDeObra);
-        fechaReparacion = (DatePicker) vista.findViewById(R.id.dpFechaReparacion);
-        botonFinalizar=(Button) vista.findViewById(R.id.btnFinalizar);
+        String dateSample = mantenimiento.getFecha_visita();
+        String oldFormat = "dd-MM-yyyy HH:mm:ss";
+        String newFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf1 = new SimpleDateFormat(oldFormat);
+        SimpleDateFormat sdf2 = new SimpleDateFormat(newFormat);
+        String fecha = "";
+        try {
+            fecha = sdf2.format(sdf1.parse(dateSample));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        tvFechaVisita.setText(fecha);
+
+        dateSample = mantenimiento.getFecha_maxima_endesa();
+        try {
+            fecha = sdf2.format(sdf1.parse(dateSample));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        tvFechaLimite.setText(fecha);
+
+        dateSample = mantenimiento.getFecha_aviso();
+        try {
+            fecha = sdf2.format(sdf1.parse(dateSample));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        tvUrgencia.setText(fecha);
         try {
 
             tiposReparacion = TiposReparacionesDAO.buscarTodosLosTiposReparaciones(getContext());
@@ -66,13 +104,7 @@ public class TabFragment3 extends Fragment {
             for (int i = 0; i < tiposReparacion.size(); i++) {
                 tipos[i]=tiposReparacion.get(i).getAbreviatura();
             }
-
-            tvFechaVisita.setText(mantenimiento.getFecha_visita());
-            tvFechaLimite.setText(mantenimiento.getFecha_maxima_endesa());
-            tvUrgencia.setText(mantenimiento.getFecha_aviso());
-
-
-                  } catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
