94993a70cbb3416aca1ee58a0885c074ef605e0e
QuantumA <ac28042@gmail.com>
2016-07-22 09:14:29 +0200

    botones

--- app/src/main/java/com/multimedia/aes/gestnet_sgsv2/adapter/AdaptadorAvisos.java
+++ app/src/main/java/com/multimedia/aes/gestnet_sgsv2/adapter/AdaptadorAvisos.java
@@ -5,11 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
-import com.multimedia.aes.gestnet_sgsv2.R;
-
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
