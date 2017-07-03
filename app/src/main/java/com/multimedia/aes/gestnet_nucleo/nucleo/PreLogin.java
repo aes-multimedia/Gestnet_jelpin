7ef14ef0dc50bf7ea63ceee7660e08b93a0ed2d1
Alejandro Vicente <alex_vicen96@hotmail.com>
2017-07-03 12:09:39 +0200

    PreLogin

--- app/src/main/java/com/multimedia/aes/gestnet_nucleo/nucleo/PreLogin.java
+++ app/src/main/java/com/multimedia/aes/gestnet_nucleo/nucleo/PreLogin.java
@@ -2,14 +2,56 @@ package com.multimedia.aes.gestnet_nucleo.nucleo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.multimedia.aes.gestnet_nucleo.R;
 
-public class PreLogin extends AppCompatActivity {
+public class PreLogin extends AppCompatActivity implements View.OnClickListener, TextWatcher {
+
+    private EditText etCodCliente;
+    private Button btnEnviarCodCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pre_login);
+        inicializarVariables();
+    }
+
+    private void inicializarVariables() {
+        ////EDITTEXTS////
+        etCodCliente = (EditText)findViewById(R.id.etCodCliente);
+        ////BUTTONS////
+        btnEnviarCodCliente = (Button)findViewById(R.id.btnEnviarCodCliente);
+    }
+
+    //////////@OVERRIDE METODS//////////
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+    ////Desabilita el boton si el campo codigo cliente esta vacio
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        if (etCodCliente.getText().toString().trim().isEmpty()){
+            btnEnviarCodCliente.setClickable(false);
+            btnEnviarCodCliente.setAlpha(0.5f);
+        }else{
+            btnEnviarCodCliente.setClickable(true);
+            btnEnviarCodCliente.setAlpha(1f);
+        }
+    }
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+    @Override
+    public void onClick(View v) {
+
     }
 }
