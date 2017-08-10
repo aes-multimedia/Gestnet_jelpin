b6daec103d55e770c800db184d92228c46700080
Alvaro <ac28042@gmail.com>
2017-08-10 13:23:52 +0200

    sdsd

--- app/src/main/java/com/multimedia/aes/gestnet_nucleo/fragment/TabFragment1.java
+++ app/src/main/java/com/multimedia/aes/gestnet_nucleo/fragment/TabFragment1.java
@@ -24,6 +24,9 @@ import java.util.ArrayList;
 public class TabFragment1 extends Fragment implements View.OnClickListener {
     private View vista;
     private Parte parte = null;
+    private TextView txtNumParte,txtCreadoPor,txtMaquina,txtTipoIntervencion,txtSituacionEquipo,txtDierccionTitular;
+    private EditText etNombreTitular,etDni,etTelefono1,etTelefono2,etTelefono3,etTelefono4;
+    private Button btnIniciarParte,btnClienteAusente;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,12 +46,43 @@ public class TabFragment1 extends Fragment implements View.OnClickListener {
     }
 
     private void inicializarVariables() {
+
+        //TEXT VIEWS
+        txtNumParte  = (TextView) vista.findViewById(R.id.txtNumParte);
+        txtCreadoPor= (TextView) vista.findViewById(R.id.txtCreadoPor);
+        txtMaquina = (TextView) vista.findViewById(R.id.txtMaquina);
+        txtTipoIntervencion= (TextView) vista.findViewById(R.id.txtTipoIntervencion);
+        txtSituacionEquipo = (TextView) vista.findViewById(R.id.txtSituacionEquipo);
+        txtDierccionTitular= (TextView) vista.findViewById(R.id.txtDierccionTitular);
+
+
+        //EDIT TEXTS
+
+        etNombreTitular = (EditText) vista.findViewById(R.id.etNombreTitular);
+        etDni= (EditText) vista.findViewById(R.id.etDni);
+        etTelefono1 = (EditText) vista.findViewById(R.id.etTelefono1);
+        etTelefono2= (EditText) vista.findViewById(R.id.etTelefono2);
+        etTelefono3= (EditText) vista.findViewById(R.id.etTelefono3);
+        etTelefono4= (EditText) vista.findViewById(R.id.etTelefono4);
+
+        //BOTONES
+
+        btnIniciarParte= (Button) vista.findViewById(R.id.btnIniciarParte);
+        btnClienteAusente = (Button) vista.findViewById(R.id.btnClienteAusente);
+
+
+
     }
     public void darValoresVariables(){
+
+
+
     }
 
     @Override
     public void onClick(View view) {
+
+
 
     }
 }
