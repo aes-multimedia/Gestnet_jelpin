be5b8b457b8e725558486224e2121183df019b68
Alvaro <ac28042@gmail.com>
2018-02-07 17:02:19 +0100

    pruebas

--- app/src/main/java/com/multimedia/aes/gestnet_nucleo/fragment/CrearArticuloDialogFragment.java
+++ app/src/main/java/com/multimedia/aes/gestnet_nucleo/fragment/CrearArticuloDialogFragment.java
@@ -27,25 +27,48 @@ public class CrearArticuloDialogFragment extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
 
-        nombre=etNombre.getText().toString();
-        unidades=Integer.parseInt(etUnidades.getText().toString());
-        iva=Integer.parseInt(etIva.getText().toString());
-        cantidadStock=Integer.parseInt(etCantidadStock.getText().toString());
-        precio=Float.parseFloat(etPrecio.getText().toString());
-        coste=Float.parseFloat(etCoste.getText().toString());
 
-        ArticuloDAO.newArticulo(getContext(),
-                0,nombre,cantidadStock,"",
-                "","","","",
-                0,iva,precio,0,coste,"",0);
+
+
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
         builder.setView(inflater.inflate(R.layout.crear_articulo_dialog, null))
+
                 // Add action buttons
                 .setPositiveButton(R.string.crear, new DialogInterface.OnClickListener() {
+
+
+
+
+
+
+
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
+
+                        etNombre= (EditText)getView().findViewById(R.id.etNombre);
+                        etUnidades= (EditText)getView().findViewById(R.id.etUnidades);
+                        etPrecio= (EditText)getView().findViewById(R.id.etPrecio);
+                        etCoste= (EditText)getView().findViewById(R.id.etCoste);
+                        etIva= (EditText)getView().findViewById(R.id.etIva);
+                        etCantidadStock= (EditText)getView().findViewById(R.id.etCantidadStock);
+
+
+                        nombre=etNombre.getText().toString();
+                        unidades=Integer.parseInt(etUnidades.getText().toString());
+                        iva=Integer.parseInt(etIva.getText().toString());
+                        cantidadStock=Integer.parseInt(etCantidadStock.getText().toString());
+                        precio=Float.parseFloat(etPrecio.getText().toString());
+                        coste=Float.parseFloat(etCoste.getText().toString());
+
+                        ArticuloDAO.newArticulo(getContext(),
+                                0,nombre,cantidadStock,"",
+                                "","","","",
+                                0,iva,precio,0,coste,"",0);
+
+
+
 
                     }
                 })
