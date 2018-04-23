3e8e6035b77aefdf932543231d1e20ad84178c78
Raul San Roman <rvsr89@gmail.com>
2018-04-23 12:43:56 +0200

    Nuevo punto en la api

--- app/src/main/java/com/multimedia/aes/gestnet_nucleo/constantes/Constantes.java
+++ app/src/main/java/com/multimedia/aes/gestnet_nucleo/constantes/Constantes.java
@@ -15,6 +15,7 @@ public class Constantes {
     public static final String URL_GEOPOS_EXTERNAPRUEBAS = "/api-gestnet-datos/v1/partes/cargaPosicion";
     public static final String URL_ARTICULOS_EXTERNAPRUEBAS = "/api-gestnet-datos/v1/partes/articulosEntidad";
     public static final String URL_PARTES_FECHA = "/api-gestnet-datos/v1/partes/listarPartesPorFecha";
+    public static final String URL_PARTES_FK_MAQUINA = "/api-gestnet-datos/v1/partes/listarPartesPorFkMaquina";
     public static final String URL_ALTA_NOTIFICACIONES_EXTERNA_PRUEBAS = "/api-gestnet-datos/v1/partes/activarNotificaciones";
     public static final String URL_INICIAR_PARTE_EXTERNAPRUEBAS ="/api-gestnet-datos/v1/partes/iniciar";
     public static final String URL_CIERRE_DIA_EXTERNAPRUEBAS = "/api-gestnet-datos/v1/partes/cierreDiario";
