b8416eef0835a439afaa479c7b21bf7873bb9677
aes-multimedia <oscarblancodiaz1998@outlook.com>
2020-02-24 12:14:36 +0100

    funcion darValores InfoArticulos.java CAMBIOS COMENTADOS

--- app/src/main/java/com/multimedia/aes/gestnet_nucleo/nucleo/InfoArticulos.java
+++ app/src/main/java/com/multimedia/aes/gestnet_nucleo/nucleo/InfoArticulos.java
@@ -62,6 +62,7 @@ public class InfoArticulos  extends AppCompatActivity implements View.OnClickLis
 
 
     private void inicializarVariables(){
+
         ivFoto =  findViewById(R.id.expandedImage);
         tvTitulo = findViewById(R.id.tvTitulo);
         tvStock = findViewById(R.id.tvStock);
@@ -88,9 +89,12 @@ public class InfoArticulos  extends AppCompatActivity implements View.OnClickLis
         tvTitulo.setText(articulo.getNombre_articulo());
 
         if(articulo.getStock()<0){
-            tvStock.setText(0);
-            btnAñadirMaterial.setClickable(false);
-            btnAñadirMaterial.setEnabled(false);
+
+            // TODO CAMBIO  setText(0) X setText(String.valueOf(0)) DABA PETE Y NO INICIABA EL ACTIVITY
+            tvStock.setText(String.valueOf(0));
+            // TODO ACTIVO BT btnAñadirMaterial CUANDO EL STOCK ES 0;
+            /*btnAñadirMaterial.setClickable(false);
+            btnAñadirMaterial.setEnabled(false);*/
         }else  tvStock.setText(String.valueOf(articulo.getStock()));
 
         tvPrecio.setText(String.valueOf(articulo.getTarifa())+ "\u20ac");
